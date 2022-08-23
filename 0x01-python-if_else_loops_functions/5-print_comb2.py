d0df5ada4ea5f1dc860157a89810935acbb76806
Abel1829 <abelzewdie21@gmail.com>
2022-08-23 10:59:13 -0700

    Two digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for nums in range(0, 100):
     if nums < 10:
-        print("0{0:d}".format(nums), end=',')
+        print("0{0:d}".format(nums), end=', ')
     else:
-        print("{0:d}".format(nums), end=',')
+        print("{0:d}".format(nums), end=', ')
