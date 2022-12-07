6a94709c40038c0211765fa6756781f599071a43
Abel1829 <abelzewdie21@gmail.com>
2022-12-07 08:10:18 -0800

    corrections

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(rom_string):
+    if rom_string is None or type(rom_string) is not str:
+        return (0)
+    ro_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = 0
+    for i in range(len(rom_string)):
+        if i > 0 and ro_num[rom_string[i]] > ro_num[rom_string[i - 1]]:
+            integer += ro_num[rom_string[i]] - \
+                2 * ro_num[rom_string[i - 1]]
+        else:
+            integer += ro_num[rom_string[i]]
+    return integer
